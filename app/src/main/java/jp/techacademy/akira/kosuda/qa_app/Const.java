677197752a8d07c7468a75011877bdb3c6a97d24
sayuen0 <sayuen0@gmail.com>
2018-03-28 14:49:13 +0900

    Initial commit エラーを修正 各所のRにエラーが出ていたので、Bulid→CleanでRクラスを再生成

---
+++ app/src/main/java/jp/techacademy/akira/kosuda/qa_app/Const.java
@@ -0,0 +1,14 @@
+package jp.techacademy.akira.kosuda.qa_app;
+
+/**
+ * Created by aki on 2018/03/28.
+ */
+
+public class Const {
+
+    public static final String UsersPATH = "users"; // Firebaseにユーザの表示名を保存するパス
+    public static final String ContentsPATH = "contents"; // Firebaseに質問を保存するバス
+    public static final String AnswersPATH = "answers"; // Firebaseに解答を保存するパス
+
+    public static final String NameKEY = "name"; // Preferenceに表示名を保存する時のキー
+}
